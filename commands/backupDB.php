9c8b657da9c4ce148b2368498f43fd88162f8289
LuoJia <luojia@luojia.me>
2014-08-14 17:18:32 +0800

    继续修复上一次的遗留bug

--- commands/backupDB.php
+++ commands/backupDB.php
@@ -15,7 +15,7 @@ $dbname = dbname; //数据库名称
 // 摘自http://www.oschina.net/code/snippet_105637_14996并小作修改，我不保证可靠性
 out("开始备份");
 
-//$SQL->query("set names 'utf8'");
+$SQL->query("set names 'utf8'");
 $mysql = "set charset utf8;\r\n";
 $q1 = $SQL->query("show tables");
 
@@ -35,14 +35,14 @@ while ($t = mysqli_fetch_array($q1))
         $keys = array_map('addslashes', $keys);
         $keys = join('`,`', $keys);
         $keys = "`" . $keys . "`";
-    $mysql .=mb_convert_encoding("insert into `$table` ($keys) values\r\n", "utf-8", "auto");
+    $mysql .="insert into `$table` ($keys) values\r\n";
     while ($data = mysqli_fetch_assoc($q3))
     {
         $vals = array_values($data);
         $vals = array_map('addslashes', $vals);
         $vals = join("','", $vals);
         $vals = "'" . $vals . "'";
-        $mysql .=mb_convert_encoding("($vals),\r\n", "utf-8", "auto");
+        $mysql .="($vals),\r\n";
     } 
     $mysql=rtrim($mysql,"\r\n\,");//移除多出的逗号
     $mysql.=";\r\n";//加个分号
