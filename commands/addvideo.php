6df0487dd6a61d85116ce4f6a8d2c17b4a364157
LuoJia <luojia@luojia.me>
2014-11-30 11:41:27 +0800

    addvideo.php

--- commands/addvideo.php
+++ commands/addvideo.php
@@ -10,10 +10,14 @@ if (@$args["t"]&&@$args["url"]) {
     if (connectSQL()) {
         Global $SQL;
         $stmt = mysqli_stmt_init($SQL);
-        mysqli_stmt_prepare($stmt, "INSERT INTO  `".dbname."`.`video` (`id` ,`title` ,`address` ,`count`) VALUES (NULL ,?,?,'0');");
-        mysqli_stmt_bind_param($stmt, "ss",$args["t"], $args["url"]);
+        mysqli_stmt_prepare($stmt, "INSERT INTO  `".dbname."`.`video` (`id` ,`title` ,`address` ,`count`,`coveraddress`,`description`) VALUES (NULL ,?,?,'0');");
+        mysqli_stmt_bind_param($stmt, "ssss",$args["t"], $args["url"],$args["cv"],$args["des"]);
         mysqli_stmt_execute($stmt);
-        echo "已尝试添加,获取的视频id:".$SQL->insert_id;
+        out( "已尝试添加,获取的视频id:".$SQL->insert_id);
+        out("标题:$args['t']");
+        out("视频地址:$args['url']");
+        out("封面地址:".(@$args['cv']?$args['cv']:"无"));
+        out("描述:".(@$args['des']?$args['des']:"无"));
         }
         else{
         	echo "数据库连接错误";
