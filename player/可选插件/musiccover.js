d2df714cbf6de5e0ce32ccbb28ed30bde9fbfc56
LuoJia <luojia@luojia.me>
2015-01-17 10:58:57 +0800

    add musiccover使用说明

--- player/可选插件/musiccover.js
+++ player/可选插件/musiccover.js
@@ -1,3 +1,4 @@
+//在命令控制台使用videosetting 视频id -showcover true 命令来使视频显示封面
 EC.addEvent("VideoInfoGet",function(p){
 	console.log(p.info.options);
 	if(p.info.options.showcover===true||p.info.options.showcover==="true"){
