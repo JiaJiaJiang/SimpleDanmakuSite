78ff3392347f7f0fe1f05f1914cd1d7344195502
LuoJia <luojia@luojia.me>
2015-01-24 00:03:26 +0800

    fix editvideo命令错误

--- commands/editvideo.php
+++ commands/editvideo.php
@@ -16,11 +16,10 @@ if(hasFlag("help")){
 if (isID(@$args["id"])) {
     if (connectSQL()) {
         Global $SQL;
-        $li=" limit 0,1";
         if(@$args["t"]){
             out("重命名");
             $stmt = mysqli_stmt_init($SQL);
-            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `title` = ? WHERE  `video`.`id` =?".$li);
+            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `title` = ? WHERE  `video`.`id` =?");
             mysqli_stmt_bind_param($stmt,"si",$args["t"],$args["id"]);
             mysqli_stmt_execute($stmt);
             out("重命名结束:".(mysqli_affected_rows($SQL)==1?"成功":"失败"));
@@ -28,7 +27,7 @@ if (isID(@$args["id"])) {
         if(@$args["url"]){
             out("更新地址");
             $stmt = mysqli_stmt_init($SQL);
-            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `address` = ? WHERE  `video`.`id` =?".$li);
+            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `address` = ? WHERE  `video`.`id` =?");
             mysqli_stmt_bind_param($stmt,"si",$args["url"],$args["id"]);
             mysqli_stmt_execute($stmt);
             out("地址更新结束:".(mysqli_affected_rows($SQL)>=1?"成功":"失败"));
@@ -36,7 +35,7 @@ if (isID(@$args["id"])) {
         if(@$args["cv"]){
             out("更新封面地址");
             $stmt = mysqli_stmt_init($SQL);
-            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `coveraddress` = ? WHERE  `video`.`id` =?".$li);
+            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `coveraddress` = ? WHERE  `video`.`id` =?");
             mysqli_stmt_bind_param($stmt,"si",$args["cv"],$args["id"]);
             mysqli_stmt_execute($stmt);
             out("封面地址更新结束:".(mysqli_affected_rows($SQL)>=1?"成功":"失败"));
@@ -44,7 +43,7 @@ if (isID(@$args["id"])) {
         if(@$args["des"]){
             out("更新描述");
             $stmt = mysqli_stmt_init($SQL);
-            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `description` = ? WHERE  `video`.`id` =?".$li);
+            mysqli_stmt_prepare($stmt, "UPDATE  `video` SET  `description` = ? WHERE  `video`.`id` =?");
             mysqli_stmt_bind_param($stmt,"si",$args["des"],$args["id"]);
             mysqli_stmt_execute($stmt);
             out("描述更新结束:".(mysqli_affected_rows($SQL)>=1?"成功":"失败"));
